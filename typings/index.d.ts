82d42b4bd74adb9fd0c89acd78ebd946de097d2f
Alec Woods <ahnolds@gmail.com>
2020-08-14 16:32:58 -0400

    feat(InvalidRequests): warn if there are too many invalid rest requests
    Emit a warning periodically as the the number of invalid (401, 403,
    429) rest requests increases to allow clients to avoid the 10k invalid
    requests in 10 minutes temporary ban

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -361,6 +361,7 @@ declare module 'discord.js' {
     };
     Events: {
       RATE_LIMIT: 'rateLimit';
+      INVALID_REQUEST_WARNING: 'invalidRequestWarning';
       CLIENT_READY: 'ready';
       RESUMED: 'resumed';
       GUILD_CREATE: 'guildCreate';
@@ -2405,6 +2406,7 @@ declare module 'discord.js' {
     messageUpdate: [oldMessage: Message | PartialMessage, newMessage: Message | PartialMessage];
     presenceUpdate: [oldPresence: Presence | undefined, newPresence: Presence];
     rateLimit: [rateLimitData: RateLimitData];
+    invalidRequestWarning: [invalidRequestWarningData: InvalidRequestWarningData];
     ready: [];
     invalidated: [];
     roleCreate: [role: Role];
@@ -2428,6 +2430,7 @@ declare module 'discord.js' {
     messageCacheLifetime?: number;
     messageSweepInterval?: number;
     allowedMentions?: MessageMentionOptions;
+    invalidRequestWarningInterval?: number;
     partials?: PartialTypes[];
     restWsBridgeTimeout?: number;
     restTimeOffset?: number;
@@ -3176,6 +3179,11 @@ declare module 'discord.js' {
     path: string;
     route: string;
     global: boolean;
+  }
+
+  interface InvalidRequestWarningData {
+    count: number;
+    remainingTime: number;
   }
 
   interface RawOverwriteData {
